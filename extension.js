a0c8e26a01f989d5baf4313e4bd5e572cd555fe9
flexagoon <code@flexagoon.com>
2023-05-02 12:28:32 +0300

    style: format the code with prettier

--- extension.js
+++ extension.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
@@ -14,61 +14,69 @@ const DBUS_SCHEMA = `
 </node>`;
 
 class Extension {
-    Get() {
-        let window_list = global.get_window_actors();
-        let focusedWindow = window_list.find(window => window.meta_window.has_focus());
+  Get() {
+    let window_list = global.get_window_actors();
+    let focusedWindow = window_list.find((window) =>
+      window.meta_window.has_focus()
+    );
 
-        // Stolen from the "Window Calls" extension
-        // https://github.com/ickyicky/window-calls/blob/b5ac7abe7e7acfcced75c5139824bd434d7c8f89/extension.js#L129
-        let workspaceManager = global.workspace_manager;
-        let currentmonitor = global.display.get_current_monitor();
-        if (focusedWindow) {
-            return JSON.stringify({
-                title: focusedWindow.meta_window.get_title(),
-                wm_class: focusedWindow.meta_window.get_wm_class(),
-                wm_class_instance: focusedWindow.meta_window.get_wm_class_instance(),
-                pid: focusedWindow.meta_window.get_pid(),
-                id: focusedWindow.meta_window.get_id(),
-                width: focusedWindow.get_width(),
-                height: focusedWindow.get_height(),
-                x: focusedWindow.get_x(),
-                y: focusedWindow.get_y(),
-                focus: focusedWindow.meta_window.has_focus(),
-                in_current_workspace: focusedWindow.meta_window.located_on_workspace(workspaceManager.get_active_workspace()),
-                moveable: focusedWindow.meta_window.allows_move(),
-                resizeable: focusedWindow.meta_window.allows_resize(),
-                canclose: focusedWindow.meta_window.can_close(),
-                canmaximize: focusedWindow.meta_window.can_maximize(),
-                maximized: focusedWindow.meta_window.get_maximized(),
-                canminimize: focusedWindow.meta_window.can_minimize(),
-                display: focusedWindow.meta_window.get_display(),
-                frame_type: focusedWindow.meta_window.get_frame_type(),
-                window_type: focusedWindow.meta_window.get_window_type(),
-                layer: focusedWindow.meta_window.get_layer(),
-                monitor: focusedWindow.meta_window.get_monitor(),
-                role: focusedWindow.meta_window.get_role(),
-                area: focusedWindow.meta_window.get_work_area_current_monitor(),
-                area_all: focusedWindow.meta_window.get_work_area_all_monitors(),
-                area_cust: focusedWindow.meta_window.get_work_area_for_monitor(currentmonitor)
-            });
-        } else {
-            return '{}';
-        }
+    // Stolen from the "Window Calls" extension
+    // https://github.com/ickyicky/window-calls/blob/b5ac7abe7e7acfcced75c5139824bd434d7c8f89/extension.js#L129
+    let workspaceManager = global.workspace_manager;
+    let currentmonitor = global.display.get_current_monitor();
+    if (focusedWindow) {
+      return JSON.stringify({
+        title: focusedWindow.meta_window.get_title(),
+        wm_class: focusedWindow.meta_window.get_wm_class(),
+        wm_class_instance: focusedWindow.meta_window.get_wm_class_instance(),
+        pid: focusedWindow.meta_window.get_pid(),
+        id: focusedWindow.meta_window.get_id(),
+        width: focusedWindow.get_width(),
+        height: focusedWindow.get_height(),
+        x: focusedWindow.get_x(),
+        y: focusedWindow.get_y(),
+        focus: focusedWindow.meta_window.has_focus(),
+        in_current_workspace: focusedWindow.meta_window.located_on_workspace(
+          workspaceManager.get_active_workspace()
+        ),
+        moveable: focusedWindow.meta_window.allows_move(),
+        resizeable: focusedWindow.meta_window.allows_resize(),
+        canclose: focusedWindow.meta_window.can_close(),
+        canmaximize: focusedWindow.meta_window.can_maximize(),
+        maximized: focusedWindow.meta_window.get_maximized(),
+        canminimize: focusedWindow.meta_window.can_minimize(),
+        display: focusedWindow.meta_window.get_display(),
+        frame_type: focusedWindow.meta_window.get_frame_type(),
+        window_type: focusedWindow.meta_window.get_window_type(),
+        layer: focusedWindow.meta_window.get_layer(),
+        monitor: focusedWindow.meta_window.get_monitor(),
+        role: focusedWindow.meta_window.get_role(),
+        area: focusedWindow.meta_window.get_work_area_current_monitor(),
+        area_all: focusedWindow.meta_window.get_work_area_all_monitors(),
+        area_cust:
+          focusedWindow.meta_window.get_work_area_for_monitor(currentmonitor),
+      });
+    } else {
+      return "{}";
     }
+  }
 
-    enable() {
-        this._dbus = Gio.DBusExportedObject.wrapJSObject(DBUS_SCHEMA, this);
-        this._dbus.export(Gio.DBus.session, '/org/gnome/shell/extensions/FocusedWindow');
-    }
+  enable() {
+    this._dbus = Gio.DBusExportedObject.wrapJSObject(DBUS_SCHEMA, this);
+    this._dbus.export(
+      Gio.DBus.session,
+      "/org/gnome/shell/extensions/FocusedWindow"
+    );
+  }
 
-    disable() {
-        this._dbus.flush();
-        this._dbus.unexport();
-        delete this._dbus;
-    }
+  disable() {
+    this._dbus.flush();
+    this._dbus.unexport();
+    delete this._dbus;
+  }
 }
 
 function init() {
-    log(`initializing ${Me.metadata.name}`);
-    return new Extension();
+  log(`initializing ${Me.metadata.name}`);
+  return new Extension();
 }
